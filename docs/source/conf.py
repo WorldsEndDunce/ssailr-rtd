7d7250849af15b7da5e0f6953b6c845f732f6117
Allison Tee <71671264+WorldsEndDunce@users.noreply.github.com>
2023-10-13 18:53:32 -0700

    Update conf.py info

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SimpleSAILR'
+copyright = '2023, Allison Tee'
+author = 'Allison Tee and Celia Blanco'
 
 release = '0.1'
 version = '0.1.0'
